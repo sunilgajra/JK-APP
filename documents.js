346c2e91f5cde582f5637f97011da80b62f5b94c
sunilgajra <sunilgajra@gmail.com>
2026-05-13 19:04:44 +0530

    Add files via upload

--- documents.js
+++ documents.js
@@ -309,6 +309,7 @@ function previewScript() {
 }
 
 export function commonStyle(docClass = "") {
+  return `
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="format-detection" content="telephone=no">
